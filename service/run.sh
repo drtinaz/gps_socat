ac70f027378e61956b685573f287f31b68b952b7
Derrick <120696745+drtinaz@users.noreply.github.com>
2025-11-27 09:04:17 -0700

    Add runit service execution script for GPS monitor

---
+++ service/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Runit service execution file for the GPS monitor.
+
+# Path to your python script
+SERVICE_SCRIPT="/data/apps/gps_socat/gps_socat.py"
+
+# Execute the script. 
+# 'exec' ensures the Python process is PID 1 for the service manager.
+# '2>&1' redirects stderr to stdout, merging all logs into the pipe 
+# for multilog to capture.
+exec $SERVICE_SCRIPT 2>&1
